739755e5f955f8f11bc5cd582905ac51d1d32c2b
Aleksandr Kremlev <kremlsa@yandex.ru>
2021-05-13 18:31:58 +0700

    2 stage

--- SQLite Viewer/task/src/viewer/SQLiteViewer.java
+++ SQLite Viewer/task/src/viewer/SQLiteViewer.java
@@ -4,9 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class SQLiteViewer extends JFrame {
     JTextField nameTextField;
@@ -41,6 +38,7 @@ public class SQLiteViewer extends JFrame {
         openFileButton = new JButton("Open");
         openFileButton.setName("OpenFileButton");
         add(openFileButton);
+        //add listener for button
         openFileButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 openFileButtonClick();
@@ -52,6 +50,12 @@ public class SQLiteViewer extends JFrame {
         comboBox.setPreferredSize(new Dimension(550,25));
         comboBox.setName("TablesComboBox");
         add(comboBox);
+        //add listener for combobox
+        comboBox.addActionListener (new ActionListener () {
+            public void actionPerformed(ActionEvent e) {
+                generateQuery();
+            }
+        });
 
         //add text area
         textArea = new JTextArea("", 20, 50);
@@ -65,17 +69,16 @@ public class SQLiteViewer extends JFrame {
 
     }
 
+    private void generateQuery() {
+        textArea.setText(String.format("SELECT * FROM %s;", comboBox.getSelectedItem()));
+    }
+
     private void openFileButtonClick() {
         String dbname = nameTextField.getText();
         dbController = new DBController();
-        ResultSet tables =dbController.setUpDBConnection(dbname);
-            try {
-                System.out.println(tables.getString("name") + "*********");
-                comboBox.addItem(tables.getString("name"));
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
-
+        comboBox.removeAllItems();
+        dbController.setUpDBConnection(dbname)
+                .forEach(comboBox::addItem);
     }
 
 }
